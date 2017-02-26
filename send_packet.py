31acd358d714b60a288d717df8029de1eca47621
Saint Louis University Rocket Propulsion <parksrocketry@slu.edu>
2017-02-26 17:12:17 -0600

    Create send_packet.py

---
+++ send_packet.py
@@ -0,0 +1,25 @@
+@@ -0,0 +1,23 @@
+ #!/usr/bin/env python
+ #http://www.instructables.com/id/Read-and-write-from-serial-port-with-Raspberry-Pi/    
+      
+           import time
+           import serial
+          
+      
+           ser = serial.Serial(
+              
+               port='/dev/ttyAMA0',
+               baudrate = 9600,
+               parity=serial.PARITY_NONE,
+               stopbits=serial.STOPBITS_ONE,
+               bytesize=serial.EIGHTBITS,
+               timeout=1
+           )
+           counter=0
+          
+      
+           while 1:
+               ser.write('Write counter: %d \n'%(counter))
+               time.sleep(1)
+               counter += 1
+\ No newline at end of file
